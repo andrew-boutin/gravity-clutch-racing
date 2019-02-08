8f82e592587a29e070623092d1f169803c62cc9c
andrew-boutin <andrew.w.boutin@gmail.com>
2019-02-07 21:29:10 -0500

    Respawn resets gravity direction and velocity too.

--- Assets/Respawnable.cs
+++ Assets/Respawnable.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 
 // Add this to an object if you want it to have the ability to be respawned.
 public class Respawnable : MonoBehaviour {
-	// TODO: Could also reset gravity on respawn if ShiftableGravity present.
-	// TODO: If using a rigidbody would also have to `rb.velocity = Vector3.zero;`.
-
 	private Vector3 spawnLocation;
 	private Quaternion spawnRotation;
+	private Rigidbody rb;
+	private GravityShiftable gravityShiftable;
 
 	void Awake () {
 		spawnLocation = this.transform.position;
 		spawnRotation = this.transform.rotation;
+		rb = GetComponent<Rigidbody> ();
+		gravityShiftable = GetComponent<GravityShiftable> ();
 	}
 
 	public void Respawn() {
 		this.transform.position = spawnLocation;
 		this.transform.rotation = spawnRotation;
+
+		if (rb) {
+			rb.velocity = Vector3.zero;
+			rb.angularVelocity = Vector3.zero;
+		}
+		if (gravityShiftable) {
+			gravityShiftable.ResetGravityDirection ();
+		}
 	}
 
 	public void UpdateRespawn(Vector3 targetSpawnLocation, Quaternion targetSpawnRotation) {
